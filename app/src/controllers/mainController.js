1389dc775f565611d74febf4f688af00a1855864
David Leonard <dleonard77@me.com>
2016-02-12 11:38:54 -0800

    Initial commit

---
+++ app/src/controllers/mainController.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Html5IonicTemplate.controller:MainController
+ * @description
+ * # MainController
+ */
+module.exports = [
+    '$scope',
+
+    function( $scope )
+    {
+        // do something with $scope
+    }
+];
